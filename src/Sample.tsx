85512d944cb393e6550f246256d7839f203438dc
YutaroKoi <koi.yutaro@gmail.com>
2021-09-11 18:23:27 +0900

    feat: routing確認用のサンプルファイル追加

---
+++ src/Sample.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const Sample: React.FC = () => {
+  return <Wrapper>Sample</Wrapper>;
+};
+
+const Wrapper = styled.div``;
